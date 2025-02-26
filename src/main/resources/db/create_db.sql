55acab250fa275c05cce788e9bcafba7520ed1be
Juan Kineipe <juan.kineipe@gmail.com>
2025-02-26 20:14:19 -0300

    update db scripts

--- src/main/resources/db/create_db.sql
+++ src/main/resources/db/create_db.sql
@@ -1,33 +1,33 @@
 CREATE DATABASE finance_manager;
 USE finance_manager;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(255) DEFAULT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `account_non_expired` bit(1) DEFAULT NULL,
-  `account_non_locked` bit(1) DEFAULT NULL,
-  `credentials_non_expired` bit(1) DEFAULT NULL,
-  `enabled` bit(1) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_user_name` (`user_name`)
+CREATE TABLE IF NOT EXISTS users (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  user_name varchar(255) DEFAULT NULL,
+  first_name varchar(255) DEFAULT NULL,
+  last_name varchar(255) DEFAULT NULL,
+  password varchar(255) DEFAULT NULL,
+  account_non_expired bit(1) DEFAULT NULL,
+  account_non_locked bit(1) DEFAULT NULL,
+  credentials_non_expired bit(1) DEFAULT NULL,
+  enabled bit(1) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY uk_user_name (user_name)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `permission` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS permission (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  description varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `user_permission` (
-  `id_user` bigint(20) NOT NULL,
-  `id_permission` bigint(20) NOT NULL,
-  PRIMARY KEY (`id_user`,`id_permission`),
-  KEY `fk_user_permission_permission` (`id_permission`),
-  CONSTRAINT `fk_user_permission` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
-  CONSTRAINT `fk_user_permission_permission` FOREIGN KEY (`id_permission`) REFERENCES `permission` (`id`)
+CREATE TABLE IF NOT EXISTS user_permission (
+  id_user bigint(20) NOT NULL,
+  id_permission bigint(20) NOT NULL,
+  PRIMARY KEY (id_user,id_permission),
+  KEY fk_user_permission_permission (id_permission),
+  CONSTRAINT fk_user_permission FOREIGN KEY (id_user) REFERENCES users (id),
+  CONSTRAINT fk_user_permission_permission FOREIGN KEY (id_permission) REFERENCES permission (id)
 ) ENGINE=InnoDB;
 
 create table accounts (id bigint not null auto_increment, balance float(53), name varchar(255), type enum ('CHECKING','CREDIT','SAVINGS'), user_id bigint not null, primary key (id)) engine=InnoDB;
@@ -40,6 +40,6 @@ alter table transactions add constraint fk_transactions_account_id foreign key (
 alter table transactions add constraint fk_transactions_category_id foreign key (category_id) references categories (id);
 alter table transactions add constraint fk_transactions_user_id foreign key (user_id) references users (id);
 
-create table expectations (id bigint not null auto_increment, amount float(53), description varchar(255), category_id bigint not null, user_id bigint not null, primary key (id)) engine=InnoDB;
+create table expectations (id bigint not null auto_increment, amount float(53), category_id bigint not null, user_id bigint not null, primary key (id)) engine=InnoDB;
 alter table expectations add constraint fk_expectations_category_id foreign key (category_id) references categories (id);
 alter table expectations add constraint fk_expectations_user_id foreign key (user_id) references users (id);
--- src/main/resources/db/insert_db.sql
+++ src/main/resources/db/insert_db.sql
@@ -1,10 +1,10 @@
 INSERT INTO users (user_name, first_name, last_name, password, account_non_expired, account_non_locked, credentials_non_expired, enabled) VALUES
-	('juan', 'Juan', 'Test', '1120962f58b29bcd74b19a8de89338db2e5cd7addb53f84c6c3b4fe9cc868608ef77e2b27dcb70e6', true, true, true, true),
-	('gaby', 'Gaby', 'Test', '965f0a4aa397f9e2b51cf9c7fd206e3ae53035dc1ec1dfb9062b5ef4f2e434c8f6fe58b544f72621', true, true, true, true);
+	('juan', 'Juan', 'Test', '$2a$10$8791PPc0iYD36p.lcX.J1OZXXnAjhE28u1SSf3gJ/Od3xssgocgUO', true, true, true, true),
+	('gaby', 'Gaby', 'Test', '$2a$10$TZQDxz3E0wfost0LC7knQ.Ghog/mxjx3UjG2vob.S0mPbiBlNAIZW', true, true, true, true);
 
-INSERT INTO permission (description) VALUES ('ADMIN'), ('MANAGER'), ('COMMON_USER');
+INSERT INTO permission (description) VALUES ('ADMIN'), ('USER');
 
-INSERT INTO user_permission (id_user, id_permission) VALUES (1, 1), (1, 2), (2, 1);
+INSERT INTO user_permission (id_user, id_permission) VALUES (1, 1), (1, 2), (2, 2);
 
 INSERT INTO categories (name, type) VALUES ('Housing', 'EXPENSE');
 INSERT INTO categories (name, type) VALUES ('Groceries', 'EXPENSE');

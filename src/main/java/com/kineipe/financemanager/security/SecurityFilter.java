59c8ab663b50532caaf7258e11277ac70c9cd2ae
Juan Kineipe <juan.kineipe@gmail.com>
2025-06-09 17:28:18 -0300

    fix: remove unnecessary call

--- src/main/java/com/kineipe/financemanager/security/SecurityFilter.java
+++ src/main/java/com/kineipe/financemanager/security/SecurityFilter.java
@@ -1,13 +1,11 @@
 package com.kineipe.financemanager.security;
 
-import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -28,7 +26,6 @@ public class SecurityFilter extends OncePerRequestFilter {
 
         String token = recoverToken(request);
         if (token != null) {
-            DecodedJWT decodedJWT = tokenService.getDecodedJWT(token);
             Long userId = tokenService.extractUserId(token);
             List<String> roles = tokenService.extractRoles(token);
             List<SimpleGrantedAuthority> authorities = roles.stream()
